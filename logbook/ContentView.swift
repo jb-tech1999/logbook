2e39513023127eac0706bffc5d5593408d95ee9c
Jandre <janbadtech@gmail.com>
2025-12-25 09:30:19 +0200

    Initial Commit

---
+++ logbook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  logbook
+//
+//  Created by Jandre Badenhorst on 2025/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ logbook/logbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  logbookApp.swift
+//  logbook
+//
+//  Created by Jandre Badenhorst on 2025/12/25.
+//
+
+import SwiftUI
+
+@main
+struct logbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
